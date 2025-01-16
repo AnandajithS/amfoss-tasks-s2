1c76884dcf4eab76cb24738356ae9955d38461e9
AnandajithS <anandajiths2006@gmail.com>
2025-01-16 23:18:18 +0530

    Finished task-03

---
+++ task-03/index.js
@@ -0,0 +1,21 @@
+function Crash(){
+    new Audio('../task-03/sounds/crash.mp3').play()
+}
+function KickBass(){
+    new Audio('../task-03/sounds/kick-bass.mp3').play()
+}
+function Snare(){
+    new Audio('../task-03/sounds/snare.mp3').play()
+}
+function Tom1(){
+    new Audio('../task-03/sounds/tom-1.mp3').play()
+}
+function Tom2(){
+    new Audio('../task-03/sounds/tom-2.mp3').play()
+}
+function Tom3(){
+    new Audio('../task-03/sounds/tom-3.mp3').play()
+}
+function Tom4(){
+    new Audio('../task-03/sounds/tom-4.mp3').play()
+}
