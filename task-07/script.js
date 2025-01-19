062ed792d197fe0c6397d238e7a3993082ec43c1
AnandajithS <anandajiths2006@gmail.com>
2025-01-19 22:56:38 +0530

    Worked on task-07

---
+++ task-07/script.js
@@ -0,0 +1,60 @@
+var getweather_button=document.getElementById("getweather");
+var myweather_button=document.getElementById("myweather");
+var details=document.getElementById("details");
+const apikey='74e5f5341fd43abcd051b787bdfbce5a';
+details.style.display="none";
+
+function fetchweather(url){
+    var location=document.getElementById("location");
+    var temperature=document.getElementById("temperature");
+    var description=document.getElementById("description");
+    var maxtemp=document.getElementById("maxtemp");
+    var mintemp=document.getElementById("mintemp");
+    var humidity=document.getElementById("humidity");
+    fetch(url)
+        .then(response=>response.json())
+        .then(data=>{
+            var name=data.name;
+            var country=data.sys.country;
+            var t=data.main.temp;
+            var max_temp=data.main.temp_max;
+            var min_temp=data.main.temp_min;
+            var hum=data.main.humidity;
+            var des=data.weather[0]["main"];
+            details.style.display="block";
+            temperature.textContent=t+" \u00B0C";
+            location.textContent=`${name}, ${country}`;
+            maxtemp.textContent=`Maximum Temperature: ${max_temp}`;
+            mintemp.textContent=`Minimum Temperature: ${min_temp}`;
+            humidity.textContent=`Humidity: ${hum}%`;
+            description.textContent=des;
+        })
+        .catch(()=>{
+            details.style.display='block';
+            location.textContent='Error! Please enter a valid city name.';
+        });
+
+}
+
+myweather_button.onclick=function(){
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position){
+            var lat=position.coords.latitude;
+            var lon=position.coords.longitude;
+            var link=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apikey}&units=metric`;
+            fetchweather(link);
+        })}
+    else{
+        details.style.display='block';
+        location.textContent='An error occured. Please try again';
+    }
+    
+}
+
+getweather_button.onclick=function(){
+    var inputval=document.getElementById("inputcity").value;
+    link=`https://api.openweathermap.org/data/2.5/weather?q=${inputval}&appid=${apikey}&units=metric`;
+    fetchweather(link);
+
+
+}
